4787502fdb447040b7a7d734f9bad63a60f2edb8
fernandoum <fernanditososa@gmail.com>
2021-06-10 18:49:45 -0300

    rectificacion punto start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 cd test-network/
-test-network/./network.sh up createChannel -c mychannel -s couchdb -i 2.2
+./test-network/./network.sh up createChannel -c mychannel -s couchdb -i 2.2
 cd .. 
 cd explorer
 docker-compose up -d 
