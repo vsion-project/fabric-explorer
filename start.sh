961daf5625b363a6826ff89b613f6f2e113caa0a
fernandoum <fernanditososa@gmail.com>
2021-06-10 19:27:29 -0300

    reparacion start

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
+#
+echo "Start Network - Create Channel in Couchdb"
 cd test-network/
 ./test-network/./network.sh up createChannel -c mychannel -s couchdb -i 2.2
 cd .. 
+echo "Start Explorer"
 cd explorer
 docker-compose up -d 
 cd .. 
+echo "Start Portainer"
 cd portainer
 docker-compose up -d
 cd ..
